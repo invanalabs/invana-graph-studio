0aae13776cb1197127b30be3765abf012c8dc07b
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-09-21 17:35:48 +0530

    first commit - styling fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+// import './index.css'
+import '@/styles/globals.css'; // Adjust the path as necessary
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
