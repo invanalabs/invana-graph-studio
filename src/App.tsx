be716832e36f2497752095bf4631311b63db488d
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-09-23 04:24:28 +0530

    page not found added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ConnectPage from './components/pages/connect';
-import ModellerPage from "./components/pages/modeller"
+import ModellerPage from "./components/pages/modeller/modeller"
 import ExplorerPage from './components/pages/explorer/explorer';
 import DataManagementPage from "./components/pages/data"
 import ProtectedRoute from './components/routes/protected-routes';
+import NotFoundPage from './components/pages/not-found';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProtectedRoute><ModellerPage /></ProtectedRoute>} />
+        <Route path="/" element={<ProtectedRoute><Navigate to={"/modeller"} /></ProtectedRoute>} />
         <Route path="/connect" element={<ConnectPage />} />
         <Route path="/modeller" element={<ProtectedRoute><ModellerPage /></ProtectedRoute>} />
         <Route path="/explorer" element={<ProtectedRoute><ExplorerPage /></ProtectedRoute>} />
         <Route path="/data" element={<ProtectedRoute><DataManagementPage /></ProtectedRoute>} />
+
+        {/* Other routes */}
+        <Route path="*" element={<NotFoundPage />} />  {/* Catch-all route for 404 */}
+     
       </Routes>
     </Router>
   );
---
+++ src/components/pages/modeller/modeller.tsx
@@ -0,0 +1,13 @@
+'use client'
+import WorkspaceLayout from '../../layouts/workspace'
+import ModellerMainToolBar from './main-toolbar'
+
+
+export default function ModellerPage() {
+
+  return (
+    <WorkspaceLayout>
+      <ModellerMainToolBar></ModellerMainToolBar>
+    </WorkspaceLayout>
+  )
+}
