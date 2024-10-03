78aaee72db9a6e6f97f2ea53502a9a0a0e4c45e4
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-10-03 13:32:31 +0530

    leftsidebar state moved to store

--- src/components/pages/graphbook/graphbook.tsx
+++ src/components/pages/graphbook/graphbook.tsx
@@ -11,59 +11,64 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/componen
 import { GripVertical } from 'lucide-react'
 import LeftSideBar from '@/components/structures/left-sidebar'
 import Footer from '@/components/structures/footer'
-
+import { useAppStore } from "@/store/appStore"
+import { Notebook } from 'lucide-react'
 
 export default function GraphBookPage() {
-  const [activePage, setActivePage] = useState(0)
-  const [pages, setPages] = useState(['Page 1', 'Page 2', 'Page 3'])
-  const [showQueryConsole, setShowQueryConsole] = useState(false)
+  // const [activePage, setActivePage] = useState(0)
+  // const [pages, setPages] = useState(['Page 1', 'Page 2', 'Page 3'])
+  // const [showQueryConsole, setShowQueryConsole] = useState(false)
+  const { rightSidebar, setRightSidebar, addPage, pages, setActivePage, leftSidebar, setLeftSidebar } = useAppStore()
 
 
-  const addPage = () => {
-    const newPage = `Page ${pages.length + 1}`
-    setPages([...pages, newPage])
-    setActivePage(pages.length)
-  }
+  // const addPage = () => {
+  //   const newPage = `Page ${pages.length + 1}`
+  //   setPages([...pages, newPage])
+  //   setActivePage(pages.length)
+  // }
 
-  const [leftSidebarContent, setLeftSidebarContent] = useState<'database' | 'settings' | null>("database")
+  // const [leftSidebarContent, setLeftSidebarContent] = useState<'database' | 'settings' | null>("database")
 
   return (
     <GraphBookLayout>
 
-<div className="flex-1 flex overflow-hidden relative">
-          <ResizablePanelGroup direction="horizontal">
-            {leftSidebarContent && (
+      <div className="flex-1 flex overflow-hidden relative">
+        <ResizablePanelGroup direction="horizontal">
+          {leftSidebar && (
+            <>
+              <ResizablePanel defaultSize={22} minSize={17} maxSize={40}>
+                <LeftSideBar onClose={() => setLeftSidebar(null)}
+                  title={<div className='flex'><Notebook className='w-4 h-4 mr-2' /> Hello World</div>}>
+                  <p>content here</p>
+                </LeftSideBar>
+              </ResizablePanel>
+              <ResizableHandle withHandle>
+                <div className="w-1.5 h-full bg-border flex items-center justify-center">
+                  <GripVertical size={12} className="text-muted-foreground" />
+                </div>
+              </ResizableHandle>
+            </>
+          )}
+
+          <ResizablePanel defaultSize={leftSidebar ? 80 : 100}>
+            <div className="flex flex-col h-full border-r">
+              {/* {showQueryConsole ? (
+                <LeftSideBar />
+              ) : ( */}
               <>
-                <ResizablePanel defaultSize={22} minSize={17} maxSize={40}>
-                  <LeftSideBar  />
-                </ResizablePanel>
-                <ResizableHandle withHandle>
-                  <div className="w-1.5 h-full bg-border flex items-center justify-center">
-                    <GripVertical size={12} className="text-muted-foreground" />
-                  </div>
-                </ResizableHandle>
+                <PageListSection />
+                <PageSection />
               </>
-            )}
+              {/* )} */}
 
-            <ResizablePanel defaultSize={leftSidebarContent ? 80 : 100}>
-              <div className="flex flex-col h-full border-r">
-                {showQueryConsole ? (
-                  <LeftSideBar />
-                ) : (
-                  <>
-                    <PageListSection />
-                    <PageSection />
-                  </>
-                )}
+              <Footer />
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
 
-                <Footer                />
-              </div>
-            </ResizablePanel>
-          </ResizablePanelGroup>
+        {/* {showRightSidebar && <RightSidebar />} */}
 
-          {/* {showRightSidebar && <RightSidebar />} */}
-
-          {/* <MainBottom
+        {/* <MainBottom
             showBottomPanel={showBottomPanel}
             bottomPanelTab={bottomPanelTab}
             setBottomPanelTab={setBottomPanelTab}
@@ -71,7 +76,7 @@ export default function GraphBookPage() {
             selectedQuery={selectedQuery}
             setSelectedQuery={setSelectedQuery}
           /> */}
-        </div>
+      </div>
     </GraphBookLayout>
 
 
--- src/components/structures/left-sidebar.tsx
+++ src/components/structures/left-sidebar.tsx
@@ -1,26 +1,36 @@
 import { Copy, X } from "lucide-react"
 import { Button } from "../ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+import { ReactNode } from "react"
 
 
-export default function LeftSideBar() {
+export interface LeftSideBarProps {
+    title : ReactNode
+    onClose: () => void
+    children : ReactNode
+}
+
+export default function LeftSideBar(props: LeftSideBarProps) {
+
+    /*
+        <LeftSideBar title={<span><Copy className='w-4 h-4 mr-2' /> "Hello World" </span>} onClose={()=> alert('clicked')}>
+            <p>content here</p>
+        </LeftSideBar>
+    */
 
     return (
         <Card
             className="h-full border-r border-border flex flex-col rounded-none shadow-none">
             <CardHeader className="bg-muted py-2">
                 <CardTitle className="flex items-center justify-between ">
-                    <span className="flex items-center"><Copy className='w-4 h-4 mr-2' /> Hello World </span>
-                    <Button variant={"link"} className="!p-0 !h-0"
-                        onClick={() => console.log('Close clicked')}>
+                    <span className="flex items-center">{props.title}</span>
+                    <Button variant={"link"} className="!p-0 !h-0" onClick={props.onClose}>
                         <X className="w-4 h-4" />
                     </Button>
                 </CardTitle>
 
             </CardHeader>
-            <CardContent>
-                <p>This is the content of the left panel. You can add any components or information here.</p>
-            </CardContent>
+            <CardContent>{props.children}</CardContent>
         </Card>
     )
 }
