43e26b47e58dfdb3ce8faa1e7235db7fff7c1b79
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-10-04 15:23:12 +0530

    cleanups

--- src/components/pages/explorer/canvas-detail.tsx
+++ src/components/pages/explorer/canvas-detail.tsx
@@ -4,10 +4,7 @@ import { useGraphBookStore } from "@/store/graphBookStore"
 
 
 export default function PageSection() {
-
     const { activeCanvas, } = useGraphBookStore()
-
-    console.log("==activeCanvas", activeCanvas)
     return (
         <div className="flex-1 overflow-hidden flex relative bg-neutral-100 dark:bg-neutral-900 ">
             <h1>Page {activeCanvas.id}</h1>
