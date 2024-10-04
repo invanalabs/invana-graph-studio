715ea0bf90c5bc0650836825c7c842229643cafa
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-10-04 16:05:20 +0530

    extra files removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import ConnectPage from './components/pages/connect';
 import ModellerPage from "./components/pages/modeller/modeller"
 // import ExplorerPage from './components/pages/explorer/explorer';
-import DataManagementPage from "./components/pages/data"
+// import DataManagementPage from "./components/pages/data"
 import ProtectedRoute from './components/routes/protected-routes';
 import NotFoundPage from './components/pages/not-found';
 import "./icons/fontawesome"
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/explorer" element={<ProtectedRoute><ExplorerPage /></ProtectedRoute>} />
         <Route path="/modeller" element={<ProtectedRoute><ModellerPage /></ProtectedRoute>} />
         {/* <Route path="/explorer" element={<ProtectedRoute><ExplorerPage /></ProtectedRoute>} /> */}
-        <Route path="/data" element={<ProtectedRoute><DataManagementPage /></ProtectedRoute>} />
+        {/* <Route path="/data" element={<ProtectedRoute><DataManagementPage /></ProtectedRoute>} /> */}
 
         {/* Other routes */}
         <Route path="*" element={<NotFoundPage />} />  {/* Catch-all route for 404 */}
