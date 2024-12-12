b2f544118fc1d1d7bf18713365f319d95a055964
Turbobot <turbobot@vercel.com>
2024-12-13 00:16:29 +0530

    feat(create-turbo): apply official-starter transform

--- src/icons/fontawesome.tsx
+++
@@ -1,6 +0,0 @@
-// src/fontAwesome.ts
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
-
-// Add the specific icons to the library
-library.add(faUserAstronaut);
