0eb22a1316d4c4b3c4b0641308e9f8eda6d89e28
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-12-17 23:48:13 +0530

    @repo changed to @invana

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "@invana/design-system": path.resolve(__dirname, "src"),
     },
   },
   build: {
