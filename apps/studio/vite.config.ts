bf5bbc282837909cd64853aee8c948b49a233622
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-12-18 09:11:01 +0530

    invana studio build fixes

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -11,22 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, 'src'),
       "@invana/ui": path.resolve(__dirname, '../../packages/ui/src'),
     },
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/main.tsx'), // Entry point of your library
-      name: 'invana-studio', // Global variable name for UMD builds
-      fileName: (format) => `invana-studio.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'], // Peer dependencies
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'jsxRuntime', // Add the global variable for the runtime
-        },
-      },
-    },
-  },
+  }
 })
