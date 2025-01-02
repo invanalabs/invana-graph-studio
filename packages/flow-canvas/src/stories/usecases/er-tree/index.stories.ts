895b14222d1dd16fc807dc72394742207fa0e8ce
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-01-03 03:09:38 +0530

    datatreeNode story added

---
+++ packages/flow-canvas/src/stories/usecases/er-tree/index.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { data } from "./simple-data";
+// import { data as groupdData } from "./grouped-data";
+import FlowCanvas from '../../../app/app';
+
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Use Cases/ER Tree',
+  component: FlowCanvas,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof FlowCanvas>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Basic: Story = {
+  args: {
+    nodes: data.nodes,
+    edges: data.edges
+  },
+};
+
+
+// export const ERDriagramGrouped: Story = {
+//   name: "Grouped ER",
+//   args: {
+//     nodes: groupdData.nodes,
+//     edges: groupdData.edges
+//   },
+// };
