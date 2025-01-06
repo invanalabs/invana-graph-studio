5c344fcb3ccacb8d022d0c32eb8e1c9c8e0f18e8
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-01-07 01:02:49 +0530

    tailwind config updated

--- apps/studio/tailwind.config.js
+++ apps/studio/tailwind.config.js
@@ -3,6 +3,8 @@ import sharedConfig from '../../packages/config-tailwind/tailwind.config';
 /** @type {import('tailwindcss').Config} */
 export const presets = [sharedConfig];
 export const content = [
-  "src/**/*.{ts,tsx}",
-  "../../packages/ui/src/**/*.{ts,tsx}"
+  "./src/**/*.{ts,tsx}",
+  "../../packages/ui/src/**/*.{ts,tsx}",
+  "../../packages/canvas-flow/src/**/*.{ts,tsx}"
+
 ];
