b93ea1e782d664156cafe112b8ad7d67f8fe13a2
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-01-08 16:30:35 +0530

    locking and unlocking canvas added

--- packages/canvas-graph/src/plugins/toolbar/toolbar.tsx
+++ packages/canvas-graph/src/plugins/toolbar/toolbar.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Graph } from "@antv/g6";
 import { useGraphin } from "@antv/graphin";
-import { Button } from "@invana/ui";
+import {
+  ButtonWithTooltip, Select, SelectContent, SelectItem,
+  SelectTrigger, SelectValue, Separator
+} from "@invana/ui";
+import { Eraser, Lock, Minus, Plus, Unlock } from "lucide-react";
 
 interface ZoomControlsProps {
   graph?: Graph | null;
+  className?: string;
 }
 
-export const ZoomControls: React.FC<ZoomControlsProps> = ({ graph }) => {
+export const ZoomControls: React.FC<ZoomControlsProps> = ({ graph, className }) => {
 
   const { graph: contextGraph } = useGraphin(); // Access the graph instance from context
 
@@ -15,6 +20,15 @@ export const ZoomControls: React.FC<ZoomControlsProps> = ({ graph }) => {
   if (!graph) {
     graph = contextGraph;
   }
+
+  const getIsLocked = () => {
+    const behaviors = graph?.getBehaviors() || [];
+    return !behaviors.includes('drag-element')
+  }
+
+  const [isLocked, setIsLocked] = useState<true | false>(getIsLocked())
+
+  const zoom = graph?.getZoom() ?? 1;
 
   const zoomIn = () => {
     const currentZoom = graph?.getZoom();
@@ -32,11 +46,94 @@ export const ZoomControls: React.FC<ZoomControlsProps> = ({ graph }) => {
     graph?.fitView();
   };
 
+  const onZoomChange = (value: string) => {
+    if (value === "fitview") {
+      fitView();
+    }
+    else {
+      graph?.zoomTo(Number(value) / 100);
+    }
+  };
+
+  const eraseCanvas = () => {
+    graph?.clear();
+  }
+
+
+  const toggleLockCanvas = () => {
+    // remove drag-element from behaviours
+    const behaviors = graph?.getBehaviors() || [];
+    if (getIsLocked()) {
+      const updatedBehaviors = behaviors.filter(b => b !== 'drag-element');
+      graph?.setBehaviors(updatedBehaviors);
+      setIsLocked(false)
+    } else {
+      const updatedBehaviors = [...behaviors, 'drag-element'];
+      graph?.setBehaviors(updatedBehaviors);
+      setIsLocked(true)
+    }
+  }
+
   return (
-    <div className="zoom-controls">
-      <Button onClick={zoomIn}>Zoom In</Button>
-      <Button onClick={zoomOut}>Zoom Out</Button>
-      <Button onClick={fitView}>Fit View</Button>
+    <div className={`zoom-controls transition-colors flex items-center shadow-sm
+              bg-transparent text-card-foreground ${className || ''}`} >
+      <Select onValueChange={onZoomChange}>
+        <SelectTrigger className="h-7 w-7 border-none hover:border-none focus:border-none active:border-none
+        rounded-none ring-0 shadow-none !w-[95px] ">
+          <SelectValue placeholder={(100 * zoom).toFixed(0) + "%"} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="10">10%</SelectItem>
+          <SelectItem value="25">25%</SelectItem>
+          <SelectItem value="50">50%</SelectItem>
+          <SelectItem value="100">100%</SelectItem>
+          <SelectItem value="200">200%</SelectItem>
+          <SelectItem value="fitview">Fit View</SelectItem>
+        </SelectContent>
+      </Select>
+      <Separator orientation="vertical" className="h-4" />
+      <ButtonWithTooltip
+        variant="ghost"
+        size="icon-sm"
+        className="rounded-none"
+        onClick={() => zoomOut()}
+        tooltip={<p>Zoom out</p>}
+      >
+        <Minus className="h-4 w-4" />
+      </ButtonWithTooltip>
+      <ButtonWithTooltip
+        variant="ghost"
+        size="icon-sm"
+        className="rounded-none"
+        onClick={() => zoomIn()}
+        tooltip={<p>Zoom In</p>}
+      >
+        <Plus className="h-4 w-4" />
+      </ButtonWithTooltip>
+      <Separator orientation="vertical" className="h-4" />
+
+      <ButtonWithTooltip
+        variant="ghost"
+        size="icon-sm"
+        className="rounded-none"
+        onClick={() => eraseCanvas()}
+        tooltip={<p>Erase Canvas</p>}
+      >
+        <Eraser className="h-4 w-4" />
+      </ButtonWithTooltip>
+
+      <Separator orientation="vertical" className="h-4" />
+
+      <ButtonWithTooltip
+        variant="ghost"
+        size="icon-sm"
+        className="rounded-none"
+        onClick={() => toggleLockCanvas()}
+        tooltip={<p>{isLocked ? 'Unlock canvas' : 'Lock canvas'}</p>}
+      >
+        {isLocked ? <Unlock className="h-4 w-4" /> : <Lock className="h-4 w-4 text-gray-400" />}
+      </ButtonWithTooltip>
+
     </div>
   );
 };
